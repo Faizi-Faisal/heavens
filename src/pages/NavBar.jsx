c82372b7dca71b68fbe862762b8e6aa3eb1e98e3
Faisal A <153807433+Faizi-Faisal@users.noreply.github.com>
2024-04-30 23:27:31 +0530

    Update NavBar.jsx

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,12 +1,12 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import './NavBar.css';
 
 const NavBar = () => {
 
   return (
-    <div className='px-4 pr-4 pt-2'>
+    <div className='MHNavBar pt-2'>
         <Navbar collapseOnSelect expand="lg" className="bg-body-white">
     <Container>
       <Navbar.Brand href="#home">Heavens Living</Navbar.Brand>
@@ -15,7 +15,7 @@ const NavBar = () => {
         <Nav className="me-auto">
           
         </Nav>
-        
+
         <Nav>
           <Nav.Link href="#deets">More deets</Nav.Link>
           <Nav.Link eventKey={2} href="#memes">
