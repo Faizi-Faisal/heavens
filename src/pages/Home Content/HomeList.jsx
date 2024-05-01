510b63f7ccbd35d254b2154a7c9b66a057f31b0b
Faisal A <153807433+Faizi-Faisal@users.noreply.github.com>
2024-05-01 17:27:47 +0530

    Update HomeList.jsx

--- src/pages/Home Content/HomeList.jsx
+++ src/pages/Home Content/HomeList.jsx
@@ -21,83 +21,9 @@ const HomeList = () => {
               <p className="card-subtitle">Bommanahalli</p>
               <span className="tagbadge badge">Boys</span>
               <br></br>
-              <div className='cardButtonhome'>
-                <Button variant="success" className="custom-button">Book Now</Button>{' '}
-                <Button variant="outline-success" className="custom-button-outline">Get CallBack</Button>{' '}
+              <div className='cardtypesdetail'>
+                <p className='bedtype'><i class="fa-solid fa-bed"></i> Bed Type : </p>
               </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="homelisting col">
-          <div className="card">
-            <img
-              src="https://res.cloudinary.com/stanza-living/image/upload/f_auto,q_auto,w_600/e_improve/e_sharpen:30/f_auto,q_auto/v1657866559/Website/CMS-Uploads/lfgi58gp04lzc5ud6pqe.jpg"
-              alt="PG in Bengaluru"
-              className="card-img-top"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Heavens PG for Boys</h5>
-              <p className="card-subtitle">Bommanahalli</p>
-              <span className="tagbadge badge">Boys</span>
-              <br></br>
-              <div className='cardButtonhome'>
-                <Button variant="success" className="custom-button">Book Now</Button>{' '}
-                <Button variant="outline-success" className="custom-button-outline">Get CallBack</Button>{' '}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="homelisting col">
-          <div className="card">
-            <img
-              src="https://res.cloudinary.com/stanza-living/image/upload/f_auto,q_auto,w_600/e_improve/e_sharpen:30/f_auto,q_auto/v1657866559/Website/CMS-Uploads/lfgi58gp04lzc5ud6pqe.jpg"
-              alt="PG in Bengaluru"
-              className="card-img-top"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Heavens PG for Boys</h5>
-              <p className="card-subtitle">Bommanahalli</p>
-              <span className="tagbadge badge">Boys</span>
-              <br></br>
-              <div className='cardButtonhome'>
-                <Button variant="success" className="custom-button">Book Now</Button>{' '}
-                <Button variant="outline-success" className="custom-button-outline">Get CallBack</Button>{' '}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="homelisting col">
-          <div className="card">
-            <img
-              src="https://res.cloudinary.com/stanza-living/image/upload/f_auto,q_auto,w_600/e_improve/e_sharpen:30/f_auto,q_auto/v1657866559/Website/CMS-Uploads/lfgi58gp04lzc5ud6pqe.jpg"
-              alt="PG in Bengaluru"
-              className="card-img-top"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Heavens PG for Boys</h5>
-              <p className="card-subtitle">Bommanahalli</p>
-              <span className="tagbadge badge">Boys</span>
-              <br></br>
-              <div className='cardButtonhome'>
-                <Button variant="success" className="custom-button">Book Now</Button>{' '}
-                <Button variant="outline-success" className="custom-button-outline">Get CallBack</Button>{' '}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="homelisting col">
-          <div className="card">
-            <img
-              src="https://res.cloudinary.com/stanza-living/image/upload/f_auto,q_auto,w_600/e_improve/e_sharpen:30/f_auto,q_auto/v1657866559/Website/CMS-Uploads/lfgi58gp04lzc5ud6pqe.jpg"
-              alt="PG in Bengaluru"
-              className="card-img-top"
-            />
-            <div className="card-body">
-              <h5 className="card-title">Heavens PG for Boys</h5>
-              <p className="card-subtitle">Bommanahalli</p>
-              <span className="tagbadge badge">Boys</span>
-              <br></br>
               <div className='cardButtonhome'>
                 <Button variant="success" className="custom-button">Book Now</Button>{' '}
                 <Button variant="outline-success" className="custom-button-outline">Get CallBack</Button>{' '}
@@ -117,3 +43,4 @@ const HomeList = () => {
 }
 
 export default HomeList;
+
