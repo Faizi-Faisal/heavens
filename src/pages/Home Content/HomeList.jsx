5ebc161cbaa079cfee8aec482c8c8b73e3bbc12e
Faisal A <153807433+Faizi-Faisal@users.noreply.github.com>
2024-05-01 14:28:10 +0530

    Update HomeList.jsx

--- src/pages/Home Content/HomeList.jsx
+++ src/pages/Home Content/HomeList.jsx
@@ -10,16 +10,19 @@ const HomeList = () => {
       </div>
       <div className="homelisting">
         <div className="card">
-          <img src="https://res.cloudinary.com/stanza-living/image/upload/f_auto,q_auto,w_600/e_improve/e_sharpen:30/f_auto,q_auto/v1657866559/Website/CMS-Uploads/lfgi58gp04lzc5ud6pqe.jpg" 
-          alt="PG in Bengaluru" className="card-img-top" />
+          <img
+            src="https://res.cloudinary.com/stanza-living/image/upload/f_auto,q_auto,w_600/e_improve/e_sharpen:30/f_auto,q_auto/v1657866559/Website/CMS-Uploads/lfgi58gp04lzc5ud6pqe.jpg"
+            alt="PG in Bengaluru"
+            className="card-img-top"
+          />
           <div className="card-body">
             <h5 className="card-title">Heavens PG for Boys</h5>
             <p className="card-subtitle">Bommanahalli</p>
             <span className="tagbadge badge">Boys</span>
             <br></br>
             <div className='cardButtonhome'>
-            <Button variant="success">Success</Button>{' '}
-            <Button variant="outline-success">Success</Button>{' '}
+              <Button variant="success" className="custom-button">Book Now</Button>{' '}
+              <Button variant="outline-success" className="custom-button-outline">Get CallBack</Button>{' '}
             </div>
           </div>
         </div>
