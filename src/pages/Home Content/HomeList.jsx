570aabcea78c54b264ab740182e9c24a8251f4f4
Faisal A <153807433+Faizi-Faisal@users.noreply.github.com>
2024-05-01 13:29:26 +0530

    Update HomeList.jsx

--- src/pages/Home Content/HomeList.jsx
+++ src/pages/Home Content/HomeList.jsx
@@ -6,8 +6,19 @@ const HomeList = () => {
     <>
       <div className='Homelisttitle'>
         <h5>Our PG in Bengaluru is ready to occupy!</h5>
-      </div> 
-     </>
+      </div>
+      <div className="homelisting">
+        <div className="card">
+          <img src="https://res.cloudinary.com/stanza-living/image/upload/f_auto,q_auto,w_600/e_improve/e_sharpen:30/f_auto,q_auto/v1657866559/Website/CMS-Uploads/lfgi58gp04lzc5ud6pqe.jpg" 
+          alt="PG in Bengaluru" className="card-img-top" />
+          <div className="card-body">
+            <h5 className="card-title">Heavens PG for Boys</h5>
+            <p className="card-subtitle">Bommanahalli</p>
+            <span className="badge">Boys</span>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
