d78a09ddbff26cbadd8b100c9a0d76b61a8c50f4
Faisal A <153807433+Faizi-Faisal@users.noreply.github.com>
2024-05-01 14:49:00 +0530

    Update HomeList.jsx

--- src/pages/Home Content/HomeList.jsx
+++ src/pages/Home Content/HomeList.jsx
@@ -26,23 +26,6 @@ const HomeList = () => {
             </div>
           </div>
         </div>
-         <div className="card">
-          <img
-            src="https://res.cloudinary.com/stanza-living/image/upload/f_auto,q_auto,w_600/e_improve/e_sharpen:30/f_auto,q_auto/v1657866559/Website/CMS-Uploads/lfgi58gp04lzc5ud6pqe.jpg"
-            alt="PG in Bengaluru"
-            className="card-img-top"
-          />
-          <div className="card-body">
-            <h5 className="card-title">Heavens PG for Boys</h5>
-            <p className="card-subtitle">Bommanahalli</p>
-            <span className="tagbadge badge">Boys</span>
-            <br></br>
-            <div className='cardButtonhome'>
-              <Button variant="success" className="custom-button">Book Now</Button>{' '}
-              <Button variant="outline-success" className="custom-button-outline">Get CallBack</Button>{' '}
-            </div>
-          </div>
-        </div>
       </div>
     </>
   );
