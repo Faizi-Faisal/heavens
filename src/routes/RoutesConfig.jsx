381ef21c0ddb88e34ff697037df3e36595660664
Faisal A <153807433+Faizi-Faisal@users.noreply.github.com>
2024-05-02 14:32:30 +0530

    Update RoutesConfig.jsx

--- src/routes/RoutesConfig.jsx
+++ src/routes/RoutesConfig.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
+import MUserLogin from '../pages/Mobile user login/MUserLogin';
 
 const RoutesConfig = (props) => {
     return (
         <Routes>
             <Route path="/" element={<Home />} /> {/* Default route */}
+            <Route path="/mobileloginuser" element={<MUserLogin />} />
         </Routes>
     );
 };
